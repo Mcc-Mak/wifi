1e5b96ac9ea242b62361a9d5a461fdccff63abd6
Martin Chun Chi MAK <ccmak@ad.hko.hksarg>
2023-06-01 16:07:41 +0800

    [auto] git-push

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 $(document).ready(async function() {
     console.log(`[index.js] init...`);
 	
-    var api = {
-       wifi: `./wifi.json`,
-    }
-    let response = await $.get(api.wifi);
-	console.log(response);
-	console.log(`response: {response.text}`);
-	console.log(JSON.parse(response.text));
+    // var api = {
+       // wifi: `./wifi.json`,
+    // }
+    // let response = await $.get(api.wifi);
+	// console.log(response);
+	// console.log(`response: {response.text}`);
+	// console.log(JSON.parse(response.text));
+	
+	async function getAjax(i_url = "") {
+		return await $.ajax(i_url);
+	}
+
+	(
+		async () => {
+			const r = await getAjax(i_url = `./wifi.json`);
+			console.log(">>> r");
+			console.log(r);
+			console.log(">>> r.responseText");
+			console.log(r.responseText);
+		}
+	) ();
 });
