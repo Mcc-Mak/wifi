f3280677e023e386fd16c045cffe6928de1f519f
Mcc-Mak <90912729+Mcc-Mak@users.noreply.github.com>
2023-06-01 14:32:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(async function() {
+    console.log(`[index.js] init...`);
+    var api = {
+        wifi: `https://www.ogcio.gov.hk/en/our_work/community/common_wifi_branding/fixed-wi-fi-hk-locations.json`,
+    }
+    let response = await $.get(api.wifi);
+    console.log(response);
+});
