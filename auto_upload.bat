e43f34ae6e07fd4041d5411888ba534520abc392
Martin Chun Chi MAK <ccmak@ad.hko.hksarg>
2023-06-01 15:36:53 +0800

    [auto] git-push

---
+++ auto_upload.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "[auto] git-push"
+git push https://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@github.com/Mcc-Mak/wifi.git
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(async function() {
     console.log(`[index.js] init...`);
-    //var api = {
-    //    wifi: `https://www.ogcio.gov.hk/en/our_work/community/common_wifi_branding/fixed-wi-fi-hk-locations.json`,
-    //}
-    //let response = await $.get(api.wifi);
-    //console.log(response);
+	
+    var api = {
+       wifi: `/wifi.json`,
+    }
+    let response = await $.get(api.wifi);
+	console.log(`response: {response.text}`);
+	console.log(JSON.parse(response.text));
 });
