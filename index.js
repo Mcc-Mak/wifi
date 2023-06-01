f6efaa8b5e67b739202e4b1523c074018069c9ee
Martin Chun Chi MAK <ccmak@ad.hko.hksarg>
2023-06-01 16:39:55 +0800

    [auto] git-push

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ async function getAjax(i_url = "") {
 $(document).ready(async function() {
     console.log(`[index.js] init...`);
 
-	const response = await getAjax(`./fixed-wi-fi-hk-locations.json`);
-	console.log(
-		JSON.parse(response.trim())
-	);
+	const response = await getAjax(`./fixed-wi-fi-hk-locations.json`);	
+	//define data array
+	var tabledata = JSON.parse(response.trim());
+	console.log(tabledata);
+
+	//initialize table
+	var table = new Tabulator("#example-table", {
+		data: JSON.parse(response.trim()), //assign data to table
+		autoColumns:true, //create columns from data field names
+	});
 });
