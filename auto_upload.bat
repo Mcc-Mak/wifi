8422f10bf0ecfe6e24c4be17731dd66ca83a8001
Martin Chun Chi MAK <ccmak@ad.hko.hksarg>
2023-06-02 17:23:29 +0800

    [auto] git-push

--- auto_upload.bat
+++ auto_upload.bat
@@ -4,7 +4,7 @@ curl https://www.ogcio.gov.hk/en/our_work/community/common_wifi_branding/non-fix
 set timestamp_now=%date:~6,4%-%date:~0,2%-%date:~3,2%T%time:~0,8%
 echo %timestamp_now: =0% > modified_datetime.log
 git add -A
-
+REM git reset -- auto_upload.bat
 git commit -m "[auto] git-push"
 git push https://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@github.com/Mcc-Mak/wifi.git
 @echo on
