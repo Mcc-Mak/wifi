2b9b9e94a816469db633f55c1e9324758b4dde40
Martin Chun Chi MAK <ccmak@ad.hko.hksarg>
2023-06-02 17:23:00 +0800

    [auto] git-push

--- auto_upload.bat
+++ auto_upload.bat
@@ -1,3 +1,10 @@
+@echo off
+curl https://www.ogcio.gov.hk/en/our_work/community/common_wifi_branding/fixed-wi-fi-hk-locations.json -o fixed-wi-fi-hk-locations.json
+curl https://www.ogcio.gov.hk/en/our_work/community/common_wifi_branding/non-fixed-wi-fi-hk-locations.json -o non-fixed-wi-fi-hk-locations.json
+set timestamp_now=%date:~6,4%-%date:~0,2%-%date:~3,2%T%time:~0,8%
+echo %timestamp_now: =0% > modified_datetime.log
 git add -A
+
 git commit -m "[auto] git-push"
-git push https://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@github.com/Mcc-Mak/wifi.git
+git push https://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@github.com/Mcc-Mak/wifi.git
+@echo on
